1c24394b2f9a17ab6338a0bf5c1369db75d11e0d
Albert Siddhartha Slawinski <errendir@gmail.com>
2017-11-24 12:09:03 -0800

    Compare tsc watch against webpack+awesome-typescript-loader

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 //import { List } from './expensive'
+//import { List } from './smallExpensive'
 import { SuperType, SubType } from './smallExpensive1'
 
-//declare var console: any
+declare var console: any
+console.log("ACD")
 
 // function test<T>(a: SuperType<T>) {
 //   const b: SubType<T> = a
